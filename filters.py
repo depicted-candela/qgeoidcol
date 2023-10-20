c9d61542a46bc4ea05e7a8fa6ed81d5cd003c664
Depiction <nicalcoca@gmail.com>
2023-10-19 21:34:29 -0500

    Terminación de filtro de Carson y gráfico para comparar dos variables de la misma línea

--- filters.py
+++ filters.py
@@ -1,5 +1,6 @@
 from .models import AeroRawProject, RawProject, TerrainRawProject
 from .string_tools import split_text_in_equal_lines as stiql
+from .ordenador import *
 
 import numpy as np
 
@@ -14,8 +15,7 @@ class Filtros:
         filtrador = get_filtros(prj, metodo)
         fil_df = filtrador(prj, kwargs)
         
-        return fil_df
-        # return prj.set_df_file_tipo(fil_df, prj.file, prj.tipo)
+        return prj.set_df_file_tipo(fil_df, prj.file, prj.tipo)
 
 
 def get_filtros(prj, metodo):
@@ -84,9 +84,8 @@ def _filtro_carson(prj, kwargs):
         var = kwargs["var"]
         w = kwargs["w0"]
         f = kwargs["f"]
-        print(var, w, f)
     except:
-        raise ValueError(f"Las variables en {kwargs.values} deben ser especifiadas como 'haz' y 'resorte'")
+        raise ValueError(f"Las variables en {kwargs.values} deben ser especifiadas como 'var', 'w0' y 'f'")
 
     if (var not in prj.df.columns): raise ValueError(f"La variable {var} no están en los datos del objeto")
     if (type(w) != int): raise ValueError(f"La variable {w} debe ser de tipo int")
@@ -97,19 +96,34 @@ def _filtro_carson(prj, kwargs):
     ## Performs the filter by Carson
     w0 = 1 / w
     a = (w0 * w) / 2
-    raw = __ida_carson(prj.groups, prj.aggregator, prj.df, a, w, f, var)
-    raw = __vuelta_carson(prj.groups, prj.aggregator, prj.df, a, w, f, var)
+    df = prj.df
+    ordenador = Ordenador()
+    
+    ## Veces para filtrar
+    raw = __ida_carson(prj.groups, prj.aggregator, df, a, w, var)
+    df = ordenador.ordenar(df, 'groups_to_df', 0, agrupador=prj.aggregator, dic=raw, var=var+"_CARSON_F")
+    raw = __vuelta_carson(prj.groups, prj.aggregator, df, a, w, var+"_CARSON_F")
+    df = ordenador.ordenar(df, 'groups_to_df', 0, agrupador=prj.aggregator, dic=raw, var=var+"_CARSON_F")
+    
+    ## Si es más de un filtro
+    if f > 1:
+        for j in range(f - 1):
+            raw = __ida_carson(prj.groups, prj.aggregator, df, a, w, var+"_CARSON_F")
+            df = ordenador.ordenar(df, 'groups_to_df', 0, agrupador=prj.aggregator, dic=raw, var=var+"_CARSON_F")
+            raw = __vuelta_carson(prj.groups, prj.aggregator, df, a, w, var+"_CARSON_F")
+            df = ordenador.ordenar(df, 'groups_to_df', 0, agrupador=prj.aggregator, dic=raw, var=var+"_CARSON_F")
+            raw = __ida_carson(prj.groups, prj.aggregator, df, a, w, var+"_CARSON_F")
+            df = ordenador.ordenar(df, 'groups_to_df', 0, agrupador=prj.aggregator, dic=raw, var=var+"_CARSON_F")
+            raw = __vuelta_carson(prj.groups, prj.aggregator, df, a, w, var+"_CARSON_F")
+            df = ordenador.ordenar(df, 'groups_to_df', 0, agrupador=prj.aggregator, dic=raw, var=var+"_CARSON_F")
 
-    # tempdf = prj.df
-    # tempdf[var+'_CARSON_F'] = raw
-
-    # return tempdf
-    return raw
+    ## Ordena el filtro en el data frame
+    return df
 
 ## Filtra con Carson desde el principio
-def __ida_carson(gr, aggr, df, a, lag, f, var):
+def __ida_carson(gr, aggr, df, a, lag, var):
 
-    final_list = dict()
+    final_dict = dict()
 
     # Filtra por grupo
     for g in gr:
@@ -117,31 +131,28 @@ def __ida_carson(gr, aggr, df, a, lag, f, var):
         ## Segmenta por grupo
         group_df = df[df[aggr] == g][var]
         group_array = np.array(group_df)
-
-        ## Veces para filtrar
-        for j in range(f):
-            
-            # Filtro
-            for i in range(0, len(group_array) - 1):
-                if i == 0:
-                    x_b = group_array[0]
-                elif i < lag:
-                    x_b = group_array[:i].mean()
-                else:
-                    x_b = group_array[i-lag:i].mean()
-                x_n = group_array[i]
-                x_n_1 = group_array[i + 1]
-                x_b_n_1 = ((1 - a)/(1 + a)) * x_b + (a / (1 + a)) * (x_n_1 + x_n)
-                group_array[i + 1] = x_b_n_1
         
-        final_list[g] = group_array
+        # Filtro
+        for i in range(0, len(group_array) - 1):
+            if i == 0:
+                x_b = group_array[0]
+            elif i < lag:
+                x_b = group_array[:i].mean()
+            else:
+                x_b = group_array[i-lag:i].mean()
+            x_n = group_array[i]
+            x_n_1 = group_array[i + 1]
+            x_b_n_1 = ((1 - a)/(1 + a)) * x_b + (a / (1 + a)) * (x_n_1 + x_n)
+            group_array[i + 1] = x_b_n_1
+        
+        final_dict[g] = group_array
 
-    return final_list
+    return final_dict
 
 ## Filtra con Carson desde el final
-def __vuelta_carson(gr, aggr, df, a, lag, f, var):
+def __vuelta_carson(gr, aggr, df, a, lag, var):
 
-    final_list = dict()
+    final_dict = dict()
 
     # Filtra por grupo
     for g in gr:
@@ -150,20 +161,17 @@ def __vuelta_carson(gr, aggr, df, a, lag, f, var):
         group_df = df[df[aggr] == g][var]
         group_array = np.array(group_df)
 
-        ## Veces para filtrar
-        for j in range(f):
+        ## Filtro inverson de Carson
+        for j in range(-1, -len(group_array), -1):
+            if j >= -lag:
+                x_b = group_array[j:].mean()
+            else:
+                x_b = group_array[j:j+lag].mean()
+            x_n = group_array[j]
+            x_n_1 = group_array[j - 1]
+            x_b_n_1 = ((1 - a)/(1 + a)) * x_b + (a / (1 + a)) * (x_n_1 + x_n)
+            group_array[j - 1] = x_b_n_1
 
-            ## Filtro inverson de Carson
-            for j in range(-1, -len(group_array), -1):
-                if j >= -lag:
-                    x_b = group_array[j:].mean()
-                else:
-                    x_b = group_array[j:j+lag].mean()
-                x_n = group_array[j]
-                x_n_1 = group_array[j - 1]
-                x_b_n_1 = ((1 - a)/(1 + a)) * x_b + (a / (1 + a)) * (x_n_1 + x_n)
-                group_array[j - 1] = x_b_n_1
+        final_dict[g] = group_array
 
-        final_list[g] = group_array
-
-    return final_list
+    return final_dict
